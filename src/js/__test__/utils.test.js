457f8263c85422f7ffce0b598523789a3d3f3d18
Pavel Fokin <fokinpavel2007@outlook.com>
2022-03-26 09:19:15 +0300

    autotest not working

--- src/js/__test__/utils.test.js
+++ src/js/__test__/utils.test.js
@@ -2,15 +2,15 @@
 const valueForTest = {
   topLeft: 0,
   topRight: 7,
-  top: 5,
-    bottomLeft: 56,
-    bottomRight: 63,
-    bottom: 61,
+  top: 5, 
+  bottomLeft: 56,
+  bottomRight: 63,
+  bottom: 61,
   right: 16,
   left: 23,
   center: 25
 }
 
 test('test',()=>{
-
+    expect(valueForTest.top).toEqual(5);
 })
